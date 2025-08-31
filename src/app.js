994af1ac728dca41d8ee42557356e4ee395be618
Yash Mhaskar <myash100@gmail.com>
2025-08-31 17:46:59 +0530

    feat: add CORS and dotenv support, set up basic Express server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors(
+    {
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    }
+))
+
+//common middleware
+app.use(express.json({limit: "16kb"})) //allows parsing of incoming requests with json payloads
+app.use(express.urlencoded({extended:true, limit:"16kb"})) // parses URL-encoded data
+app.use(express.static("public")) //This serves static files from the public folder
+
+export { app }
