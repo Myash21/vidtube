efaab658e3beb35d184e775eaee36756b06680e7
Yash Mhaskar <myash100@gmail.com>
2025-08-31 19:35:25 +0530

    feat: implement MongoDB connection and update server initialization

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv"
+dotenv.config()
+
+const mongodb_url = process.env.mongodb_url
+const connectdb = async() => {
+    try {
+        const connection_instance = await mongoose.connect(`${mongodb_url}/${DB_NAME}`)
+        console.log(`\n MongoDB Connected! DB host: ${connection_instance.connection.host}`)
+    } catch (error) {
+        console.log("MongoDB connection error!", error)
+        process.exit(1)
+    }
+}
+
+export default connectdb
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import { app } from "./app.js";
 import dotenv from "dotenv"
+import connectdb from "./db/index.js";
 dotenv.config()
 
 const PORT = process.env.PORT
-app.listen(PORT, () => {
+connectdb()
+.then(() => {
+    app.listen(PORT, () => {
     console.log("Server is running!")
+})
+})
+.catch((err) => {
+    console.log("MongoDB connection error!", err)
 })
