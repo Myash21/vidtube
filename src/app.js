f87a7414266aea38fb689ccd18e58b97e9fa7f1d
Yash Mhaskar <myash100@gmail.com>
2025-09-02 10:43:11 +0530

    feat: add cookie-parser for handling cookies in requests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
@@ -14,6 +15,7 @@ app.use(cors(
 app.use(express.json({limit: "16kb"})) //allows parsing of incoming requests with json payloads
 app.use(express.urlencoded({extended:true, limit:"16kb"})) // parses URL-encoded data
 app.use(express.static("public")) //This serves static files from the public folder
+app.use(cookieParser())
 
 //import routes
 import healtcheckRouter from "./routes/healthcheck.routes.js"
