b759da6ebacff6432a3e9fa3b06c27fce3061604
Yash Mhaskar <myash100@gmail.com>
2025-09-04 00:05:34 +0530

    feat: add logout route with JWT verification middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { registerUser, logoutUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
 
-router.route("/register").post(
+router.route("/register").post( //.post(middleware, controller)
+    //inject upload middleware
     upload.fields([
         {
             name: "avatar",
@@ -16,5 +18,7 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+router.route("/logout").post(verifyJWT, logoutUser) //.post(middleware, controller)
 
 export default router;
