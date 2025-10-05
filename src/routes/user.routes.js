beb9327d0a1c4ab6836072b3877d10018d477525
Yash Mhaskar <myash100@gmail.com>
2025-10-05 12:54:28 +0530

    feat: add user authentication and account management routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { registerUser, logoutUser } from "../controllers/user.controllers.js";
+import { registerUser, logoutUser, loginUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, 
+    getUserChannelProfile, getWatchHistory, updateAccountDetails, updateAvatar, 
+    updateCoverImage} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
 
+//unsecured routes(anyone can access them)
 router.route("/register").post( //.post(middleware, controller)
     //inject upload middleware
     upload.fields([
@@ -19,6 +22,17 @@ router.route("/register").post( //.post(middleware, controller)
     ]),
     registerUser)
 
+router.route("/login").post(loginUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
+//secured routes
 router.route("/logout").post(verifyJWT, logoutUser) //.post(middleware, controller)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watchHistory").get(verifyJWT, getWatchHistory)
+router.route("update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar)
+router.route("/update-coverImage").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
 
 export default router;
